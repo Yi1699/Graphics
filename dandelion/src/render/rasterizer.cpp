ab508337571fc379f17df1a81fe3805d81b09aec
SuperYee <1337936093@qq.com>
2023-10-16 20:33:30 +0800

    add some comments

--- dandelion/src/render/rasterizer.cpp
+++ dandelion/src/render/rasterizer.cpp
@@ -24,7 +24,7 @@ using std::tuple;
 
 // 给定坐标(x,y)以及三角形的三个顶点坐标，判断(x,y)是否在三角形的内部（视口坐标系下）
 bool Rasterizer::inside_triangle(int x, int y, const Vector4f* vertices)
-{
+{   //计算像素点在三角形的重心坐标，当三个坐标均大于0时该点才在三角形内部
     auto [a, b, c] = compute_barycentric_2d((float)x, (float)y, vertices);
     if(a < 0 || b < 0 || c < 0) return false;
     return true;
@@ -46,33 +46,27 @@ tuple<float, float, float> Rasterizer::compute_barycentric_2d(float x, float y, 
 void Rasterizer::draw(const std::vector<Triangle>& TriangleList, const GL::Material& material,
                       const std::list<Light>& lights, const Camera& camera)
 {
-    // iterate over all triangles in TriangleList
+    // iterate over all triangles in TriangleList遍历所有三角形进行光栅化
     for (const auto& t : TriangleList) {
         Triangle newtriangle = t;
         std::array<Vector3f, 3> worldspace_pos;
         for(int i = 0; i < 3; i++)
         {
             struct VertexShaderPayload payload = {newtriangle.vertex[i], newtriangle.normal[i]};
+            //计算三角形三个顶点的世界坐标
             Vector4f temp_world = model * newtriangle.vertex[i];
             worldspace_pos[i] << temp_world.x(), temp_world.y(), temp_world.z();
             auto [out_position, out_normal] = vertex_shader(payload);
             newtriangle.vertex[i] = out_position;
             newtriangle.normal[i] = out_normal;
         }
+        //对单个三角形进行光栅化
         rasterize_triangle(newtriangle, worldspace_pos, material, lights, camera);
 
-        // transform vertex position to world space for interpolating
-
-
-        // Use vetex_shader to transform vertex attributes(position & normals) to
-        // view port and set a new triangle
-        
-
-        // call rasterize_triangle()
     }
 }
 
-// 对顶点的某一属性插值，vert为顶点的某一属性
+// 对顶点的某一属性插值，vert为顶点的某一属性，该处使用了透视插值校正算法
 Vector3f Rasterizer::interpolate(float alpha, float beta, float gamma, const Eigen::Vector3f& vert1,
                                  const Eigen::Vector3f& vert2, const Eigen::Vector3f& vert3,
                                  const Eigen::Vector3f& weight, const float& Z)
@@ -92,6 +86,7 @@ void Rasterizer::rasterize_triangle(const Triangle& t, const std::array<Vector3f
                                     Camera camera)
 {
     // discard all pixels out of the range(including x,y,z)
+    //定义光栅化遍历的边界，并将边界缩小至三角形的包围矩形，可以大大减少渲染时间
     float edgeLeft =    std::min({t.vertex[0].x(), t.vertex[1].x(), t.vertex[2].x()});
     float edgeRight =   std::max({t.vertex[0].x(), t.vertex[1].x(), t.vertex[2].x()});
     float edgeTop =     std::max({t.vertex[0].y(), t.vertex[1].y(), t.vertex[2].y()});
@@ -100,6 +95,7 @@ void Rasterizer::rasterize_triangle(const Triangle& t, const std::array<Vector3f
     if(edgeRight > width) edgeRight = (float)width;
     if(edgeBottom < 0) edgeBottom = 0.f;
     if(edgeTop > height) edgeTop = (float)height;
+    //遍历包围矩形中的所有像素，进行插值和光线计算
     for(int i = (int)edgeLeft; i < edgeRight; i++)
     {
         for(int j = (int)edgeBottom; j < edgeTop; j++)
@@ -110,25 +106,22 @@ void Rasterizer::rasterize_triangle(const Triangle& t, const std::array<Vector3f
                 auto [alpha, beta, gamma] = compute_barycentric_2d((float)i, (float)j, t.vertex);
                 float Z = 1 / (alpha / t.vertex[0].w() + beta / t.vertex[1].w() + gamma / t.vertex[2].w());
                 Vector3f weight = Vector3f{t.vertex[0].w(), t.vertex[1].w(), t.vertex[2].w()};
+                //在世界坐标系中对像素点进行插值得到该点的世界坐标
                 Vector3f newpoint = interpolate(alpha, beta, gamma, world_pos[0], world_pos[1], world_pos[2], weight, Z);
+                //OpenGL默认看向z轴负方向
                 if(-newpoint.z() < depth_buf[bufferindex])
                 {
                     depth_buf[bufferindex] = -newpoint.z();
+                    //对该点法线进行插值，得到像素点对应面片的法线的世界坐标
                     Vector3f camera_normal = interpolate(alpha, beta, gamma, t.normal[0], t.normal[1], t.normal[2], weight, Z);
-                    camera_normal.normalize();
+                    camera_normal.normalize();//法线需要归一化
                     struct FragmentShaderPayload payload = {newpoint, camera_normal};
+                    //调用phong-shader计算面片颜色
                     frame_buf[bufferindex] = phong_fragment_shader(payload, material, lights, camera);
                 }
             }
         }
     }
-    
-    // if current pixel is in current triange:
-    // 1. interpolate depth(use projection correction algorithm)
-    // 2. interpolate vertex positon & normal(use function:interpolate())
-    // 3. fragment shading(use function:fragment_shader())
-    // 4. set pixel
-
 }
 
 // 初始化整个光栅化渲染器
--- dandelion/src/render/shader.cpp
+++ dandelion/src/render/shader.cpp
@@ -23,22 +23,20 @@ VertexShaderPayload vertex_shader(const VertexShaderPayload& payload)
     // Vertex position transformation顶点位置变换到视口坐标系
 
     output_payload.position =Uniforms::MVP * output_payload.position;
-
+    //视口变换矩阵的修改版，由于w的值并不是1，故坐标需除以w的值，经视口变换后坐标变换至屏幕坐标系
     Eigen::Matrix4f viewport = Eigen::Matrix4f::Zero();
     viewport(0, 0) = Uniforms::width/2.f/output_payload.position.w();
     viewport(1, 1) = Uniforms::height/2.f/output_payload.position.w();
     viewport(2, 2) = 1;
     viewport(3, 3) = 1;
-    
     Vector4f vv = {Uniforms::width/2.f, Uniforms::height/2.f, 0, 0};
     output_payload.position =viewport * output_payload.position + vv;
+    //法线变换到世界坐标系
     Vector4f tempnormal = {output_payload.normal[0], output_payload.normal[1], output_payload.normal[2], 0};
     tempnormal = Uniforms::inv_trans_M * tempnormal;
     output_payload.normal = {tempnormal[0], tempnormal[1], tempnormal[2]};
     output_payload.normal.normalize();
-
-    // Vertex normal transformation法线变换到世界坐标系
-
+    
     return output_payload;
 }
 
@@ -46,32 +44,35 @@ Vector3f phong_fragment_shader(const FragmentShaderPayload& payload, GL::Materia
                                const std::list<Light>& lights, Camera camera)
 {
     Vector3f result = {0, 0, 0};
+    //计算相机相对面片的向量并归一化
     Vector3f vi = camera.position - payload.world_pos;
- 
     vi.normalize();
     float Iam = 0.1f;
     float p = material.shininess;
     // ka,kd,ks can be got from material.ambient,material.diffuse,material.specular
     Vector3f ka = material.ambient, kd = material.diffuse, ks = material.specular;
-//    std::cout<<ks[0]<<" "<<ks[1]<<" "<<ks[2]<<std::endl;
     Vector3f Ls = {0, 0, 0};
     Vector3f Ld = {0, 0, 0};
+    //利用list的迭代器，遍历每个光源进行计算
     for (std::list<Light>::const_iterator it = lights.begin(); it != lights.end(); ++it) {
+        //光线的单位向量并计算光程
         Vector3f li = it->position - payload.world_pos;
- 
         float Light_length = li.norm();
         li.normalize();
+        //Blinn-phong算法中，计算半程向量
         Vector3f ha = (vi + li) / (vi + li).norm();
+        //计算衰减光
         float attenuated_light = it->intensity / std::pow(Light_length, 2.0f);
+        //漫反射光线
         Ld = Ld + kd * (attenuated_light) * std::max(0.0f, payload.world_normal.dot(li));
+        //镜面反射光线
         Ls = Ls + ks * (attenuated_light) * std::pow(std::max(0.0f, payload.world_normal.dot(ha)), p);
     }
-
+    //结果以RGB表示，并加上三种光，需判断是否超出255，超出直接赋值255
     result = (Ls + Ld + ka * Iam) * 255.f;
     for(int i = 0; i < 3; i++)
     {
         if(result[i] > 255) result[i] = 255.f;
     }
-
     return result;
 }
